9a2be8cc6e21029b312baf326a23d82d27822872
henryducthinh <72155787+henryducthinh@users.noreply.github.com>
2020-12-06 18:17:49 +0700

    Update fx-cander.js

--- JS/fx-cander.js
+++ JS/fx-cander.js
@@ -5,7 +5,7 @@ var createCalendarWidget = function(a) {
             k = parseInt(a.mode) || 1,
             d = a.dateformat,
             g, e = "";
-        b.lang = "en";
+        b.lang = "vi";
         b.containerId = a.id || "economicCalendarWidget";
         if (a = a.lang || document.documentElement.lang) a = a.replace(/\s/g, "").substring(0, 2).toLowerCase(), -1 !== "ru,en,zh,es,pt,ja,de,tr".indexOf(a) && (b.lang = a);
         e = window.location.hostname;
@@ -36,8 +36,12 @@ var createCalendarWidget = function(a) {
             var f = !1,
                 c, d;
             if (document.addEventListener) document.addEventListener("DOMContentLoaded", a, !1), window.addEventListener("load", a, !1);
-            else if (window.attachEvent) { window.attachEvent("onload", a); var e = document.createElement("div"); try { var h = null === window.frameElement } catch (l) {}
-                e.doScroll && h && window.external && (d = setInterval(function() { try { e.doScroll(), a() } catch (l) {} }, 30)) }
+            else if (window.attachEvent) {
+                window.attachEvent("onload", a);
+                var e = document.createElement("div");
+                try { var h = null === window.frameElement } catch (l) {}
+                e.doScroll && h && window.external && (d = setInterval(function() { try { e.doScroll(), a() } catch (l) {} }, 30))
+            }
             "complete" === document.readyState && a()
         }
     },
@@ -46,12 +50,18 @@ var createCalendarWidget = function(a) {
             arguments);
         this.create()
     },
-    economicCalendarEvent = function(a) { createCalendarWidget.apply(this, arguments); var b = this;
+    economicCalendarEvent = function(a) {
+        createCalendarWidget.apply(this, arguments);
+        var b = this;
         this.mode = parseInt(a.mode) || 0;
         this.showTitle = parseInt(a.showTitle) || !1;
         this.id = parseInt(a.id) || 0;
         this.containerId = "economicCalendarEventWidget";
-        this.createSrc = function() { var a = "";
+        this.createSrc = function() {
+            var a = "";
             b.mode && (a += "?displayMode=" + b.mode);
-            b.showTitle && (a += (b.mode ? "&" : "?") + "showTitle=1"); return "https://www.tradays.com/" + b.lang + "/economic-calendar/widget/event/" + b.id + a };
-        this.create() };
+            b.showTitle && (a += (b.mode ? "&" : "?") + "showTitle=1");
+            return "https://www.tradays.com/" + b.lang + "/economic-calendar/widget/event/" + b.id + a
+        };
+        this.create()
+    };
