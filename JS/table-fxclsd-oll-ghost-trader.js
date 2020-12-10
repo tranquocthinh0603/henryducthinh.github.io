4ff57404cec358b605aceedf450841560a3a9e09
henryducthinh <72155787+henryducthinh@users.noreply.github.com>
2020-12-10 18:21:28 +0700

    Update table-fxclsd-oll-ghost-trader.js

--- JS/table-fxclsd-oll-ghost-trader.js
+++ JS/table-fxclsd-oll-ghost-trader.js
@@ -8,7 +8,6 @@ var spData = null;
 function doData(json) {
     spData = json.feed.entry;
     //nếu cần kiểm tra thì thử console.log phát xem dữ liệu đang có dạng như nào nhé
-    console.log(spData);
 }
 
 function readData() {
