de03bc4431090b85137352f2a02558fced4b246c
henryducthinh <72155787+henryducthinh@users.noreply.github.com>
2020-12-10 18:04:09 +0700

    Update table-fxclsd-oll-ghost-trader.js

--- JS/table-fxclsd-oll-ghost-trader.js
+++ JS/table-fxclsd-oll-ghost-trader.js
@@ -14,6 +14,34 @@ function doData(json) {
 function readData() {
     var data = spData;
     var strData = '';
+    for (var r = 0; r < data.length; r++) {
+        var cell = data[r]["gs$cell"]; //gán nội dung cần lấy - gs$cell vào biến để xử lý
+        if (r >= 0 && r < 4) { // từ ô đầu tiên tới số thứ 4 xử lý riêng vì là hàng lưu tên trường dữ liệu, nếu không thích có thể bỏ qua việc ghi tên cột lên hàng 1, hoặc bỏ qua xử lý đoạn này. 
+            if (cell["col"] == "1") { //cột đầu tiên mình lưu ID
+                strData += '<tr class="row">';
+                strData += '<th class="col-md-1"><b>' + cell["$t"] + '</b></th>'; //nội dung nằm trong gs$cell->$t
+            } else if (cell["col"] == "2") { //cột thứ 2 mình lưu title
+                strData += '<th class="col-md-1"><b>' + cell["$t"] + '</b></th>';
+            } else if (cell["col"] == "3") { //cột thứ3 mình lưu link ảnh, với th thì vẫn in bình thường, nhưng xuống tới dưới sẽ in thành hình ảnh nhé
+                strData += '<th class="col-md-1"><b>' + cell["$t"] + '</b></th>';
+            } else if (cell["col"] == "4") { //cột cuối lưu mô tả ngắn cho bài
+                strData += '<th class="col-md-2"><b>' + cell["$t"] + '</b></th>';
+                strData += '</tr>';
+            }
+        } else { //xử lý các bản ghi dữ liệu bình thường
+            if (cell["col"] == "1") {
+                strData += '<tr class="row">';
+                strData += '<td class="col-md-1">' + cell["$t"] + '</td>';
+            } else if (cell["col"] == "2") {
+                strData += '<td class="col-md-1">' + cell["$t"] + '</td>';
+            } else if (cell["col"] == "3") {
+                strData += '<td class="col-md-1"><img width="100%" src="' + cell["$t"] + '"></td>';
+            } else if (cell["col"] == "4") {
+                strData += '<td class="col-md-2">' + cell["$t"] + '</td>';
+                strData += '</tr>';
+            }
+        }
+    }
     $('#data-fx').after(strData);
 }
 $(document).ready(function() {
