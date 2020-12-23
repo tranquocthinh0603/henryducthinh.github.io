85f7338019a0cc2b760285676c1db48e4f8e6553
henryducthinh <72155787+henryducthinh@users.noreply.github.com>
2020-12-23 20:19:53 +0700

    Create reload.js

---
+++ JS/reload.js
@@ -0,0 +1,12 @@
+/**
+ * Auther: Ghost Trader Index - Trần Quốc Thịnh
+ * Web : henryducthinh.github.io
+ * V - 0.1
+ */
+function reFresh() {
+
+    window.open(location.reload(true))
+
+}
+
+window.setInterval("reFresh()", ((Math.random() * 800000) + 80000));
