90823350d9a173ec11a89e39d25bd9d143967258
henryducthinh <72155787+henryducthinh@users.noreply.github.com>
2021-01-19 04:59:37 +0700

    Create media.js

---
+++ JS/media.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("menu-top");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
