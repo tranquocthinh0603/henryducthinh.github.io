944d3b66f40e44272967fea97f165914dd213b46
henryducthinh <72155787+henryducthinh@users.noreply.github.com>
2021-02-04 14:27:45 +0700

    Create conten-sp.js

---
+++ support%%strategy/conten-sp.js
@@ -0,0 +1,13 @@
+// text code supports Trader
+// Author : Ghost Trader-Index
+var t = '<b class="sub-md">Tuyên bố rủi ro! - Thông điệp gửi đến các đối tác.</b>';
+var t1 = '<b> Giao dịch thị trường tài chính ngoại hối, có nguy cơ rủi ro lớn đối với tất cả khoản vốn quý đầu tư. </b>';
+var t4 = '<b> Quý đầu tư sử dụng chiến lươc của tôi, cần xem xét thật kỉ về % xác suất và trạng thái của chiến lược. </b>';
+var t2 = '<b> Quý đầu tư hoàn toàn chịu trách nhiệm từ chiến lược giao dịch của tôi. </b>';
+var t3 = '<b> Tôi khuyến nghị khi tài khoản giao dịch của quý đầu tư. Lv margin dưới 500% thì liên hệ qua zalo <b class="sub-md"> 0949432787 </b>. Để được hỗ trợ an toàn cho tài khoản.</b>';
+var t5 = '<b> Chúc quý đầu tư giao dịch thành công!!!. </b>';
+var span = '<span class = "sp-md">';
+var span2 = '</span>';
+var p = '<p>';
+var p2 = '</p>';
+document.getElementById("ghost-sp").innerHTML = span + p + t + p2 + p + t1 + p2 + p + t4 + p2 + p + t2 + p2 + p + t3 + p2 + p + t5 + p2 + span2;
