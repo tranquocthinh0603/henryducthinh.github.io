f97c9139ae4ab86f21a561ef573e8e77de15ec87
henryducthinh <72155787+henryducthinh@users.noreply.github.com>
2022-10-31 07:49:09 +0700

    Create meta.js

---
+++ JS/meta.js
@@ -0,0 +1,29 @@
+// Meta seo google - Meta link
+var meta = '<meta name="google-site-verification" content="HlH3eDnnAXV2Uvlx2jTCsTNX2lN-6DB_2qloQcp-Ios" />';
+var meta1 = '<meta http-equiv="content-language" content="vi" />';
+var meta2 = '<meta name="robots" content="noodp,index,follow,all" />';
+var meta3 = '<meta name="googlebot" content="index,follow,all" />';
+var meta4 = '<meta name="revisit-after" content="1 days " />';
+var meta5 = '<meta name="author" content="Ghost Trader">';
+var meta6 = '<meta name="copyright" content="© 2020 Dịch vụ tài chính - FUTURE CONTRACT by https://henryducthinh.github.io/" />';
+var meta7 = '<meta name="rating" content="adult" />';
+var meta8 = '<meta name="rating" content="RTA-5042-1996-1400-1577-RTA" />';
+var meta9 = '<meta name="news_keywords" content="XAU/USD;GBP/USD;Lịch kinh tế;Investing;EUR/USD;XAG/USD" />';
+var meta10 = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+var meta11 = '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+var list = meta10 + meta11 + meta + meta1 + meta2 + meta3 + meta4 + meta5 + meta6 + meta7 + meta8 + meta9;
+// --------------------------------end--->
+//----------------index.html--starts-->
+var index = '<meta property="og:url" content="https://henryducthinh.github.io/">';
+var index1 = '<meta property="og:title" content="Dịch vụ tài chính - FUTURE CONTRACT">';
+var index2 = '<meta property="og:image" content="https://henryducthinh.github.io/upload/logo-ghost-trader-index.png" sizes="120x120" />';
+var index3 = '<meta property="og:description" content="Dịch vụ tài chính - Nghiên cứu phân tích tài chính thế giới" />';
+var index4 = '<meta property="og:type" content="article" />';
+var index5 = '<meta property="og:locale" content="vi_VN" />';
+var index6 = '<meta name="description" content="Dịch vụ tài chính - FUTURE CONTRACT">';
+var index7 = '<meta name="keywords" content="Hướng dẫn tìm hiểu thị trường chứng khoán cho người mới bắt đầu,Giới thiệu thương hiệu uy tín,sàn uy tín,chien luoc forex,0925850617,ghost.trader.index@gmail.com,chiến lược giao dịch forex,Trend fx,Tài liệu"/>';
+var index8 = '<meta name="description" content="Dịch vụ tài chính hỗ trợ lệnh giao dịch forex, tất cả sản phẩm thị trường tài chính quốc tế,XAU/USD,GPB/USD,EUR/USD...,Thương vụ tiềm năng trong tháng">';
+var index9 = '<title>Nhận định thị trường tài chính - Nghiên cứu phân tích tài chính thế giới</title>'
+var index0 = index + index1 + index2 + index3 + index4 + index5 + index6 + index7 + index8 + index9;
+//-------------------------------------index!----end--->
+document.getElementById("meta-index").innerHTML = list + index0; //----id-meta-link--content-->
